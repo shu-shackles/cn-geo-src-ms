a13e1a7eaffd95c946240e76d3b1f9f3236549a1
sherlockjohnson22333@gmail.com <sherlockjohnson22333@gmail.com>
2022-07-10 13:42:54 +0800

    数据获取修改

--- back-end/api/v1/endpoints/mineral.py
+++ back-end/api/v1/endpoints/mineral.py
@@ -5,14 +5,16 @@ from models.db import con
 mineral = APIRouter(tags=["矿产资源"])
 
 
+# 按列表方式返回所有数据
 @mineral.get("/mineralAll", summary="所有矿产数据")
 def get_all():
-    sql_result = con.execute(f'select * from mineral_resources')
+    sql_result = con.execute(f'SELECT * FROM mineral_resources')
     return sql_result.all()
 
 
-@mineral.get("/mineralSingle", summary="单条矿产数据")
+# 根据获取到的省份名称按列表方式返回对应数据
+@mineral.get("/mineralProvince", summary="省市矿产数据")
 def get_single(province):
     sql_result = con.execute(
-        f'select * from mineral_resources where province=\'{province}\'')
+        f'SELECT * FROM mineral_resources WHERE province=\'{province}\'')
     return sql_result.all()
