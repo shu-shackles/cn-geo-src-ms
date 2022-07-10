08ab27bd2584c91afb0685d22eeb81e7645166b7
jipaishangren <419540139@qq.com>
2022-07-10 11:24:26 +0800

    补充后端关于user和tag的功能，修整代码格式

--- back-end/api/v1/endpoints/login.py
+++ back-end/api/v1/endpoints/login.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from pydantic import BaseModel
 # from fastapi.security import OAuth2PasswordRequestForm
 
@@ -11,32 +11,32 @@ login = APIRouter(tags=["认证相关"])
 
 
 class LoginItem(BaseModel):
-  name: str
-  password: str
+    name: str
+    password: str
 
 
 class RegisterItem(BaseModel):
-  name: str
-  password: str
+    name: str
+    password: str
 
 
 @login.post("/login", summary="用户登录")
 async def user_login(item: LoginItem):
-  if user.confirm_user(item.name):
-    if user.is_password(item.name, item.password):
-      return "密码正确"
+    if user.confirm_user(item.name):
+        if user.is_password(item.name, item.password):
+            return "密码正确"
+        else:
+            return "密码错误"
     else:
-      return "密码错误"
-  else:
-    return "用户名不存在"
+        return "用户名不存在"
 
 
 @login.post("/register", summary="用户注册")
 async def user_register(item: RegisterItem):
-  if user.confirm_user(item.name):
-    return "用户名重复"
-  else:
-    if user.insert(item.name, item.password):
-      return "插入成功"
+    if user.confirm_user(item.name):
+        return "用户名重复"
     else:
-      return "插入失败"
+        if user.insert(item.name, item.password):
+            return "插入成功"
+        else:
+            return "插入失败"
--- back-end/api/v1/endpoints/user.py
+++ back-end/api/v1/endpoints/user.py
@@ -7,40 +7,41 @@ from models import user
 users = APIRouter(tags=["用户相关"])
 
 
-class InfoItem(BaseModel):
-  offset: int
-  count: int
+# class InfoItem(BaseModel):
+#   offset: int
+#   count: int
 
 
 class SetInfoItem(BaseModel):
-  password: str
-  type: int
-  area: str
-  uid: int
+    password: str
+    type: int
+    area: str
+    uid: int
 
 
 class DeleteItem(BaseModel):
-  uid: int
+    uid: int
 
-@users.get("/info/:offset/:count", summary="当前用户")
-async def user_info(item: InfoItem):
-  sql_result = user.user_info(item.offset, item.count)
-  data = [dict(zip(result.keys(), result)) for result in sql_result]
-  result = json.dumps(data)
-  return result
+
+@users.get("/info/{offset}/{count}", summary="当前用户")
+async def user_info(offset, count):
+    sql_result = user.user_info(offset, count)
+    data = [dict(zip(result.keys(), result)) for result in sql_result]
+    result = json.dumps(data)
+    return result
 
 
 @users.put('/setinfo', summary="修改用户")
 async def user_setinfo(item: SetInfoItem):
-  if user.user_setinfo(item.password, item.type, item.area, item.uid):
-    return "修改成功"
-  else:
-    return "修改失败"
+    if user.user_setinfo(item.password, item.type, item.area, item.uid):
+        return "修改成功"
+    else:
+        return "修改失败"
 
 
 @users.delete('/deleteuser', summary="")
 async def user_delete(item:DeleteItem):
-  if user.user_delete(item.uid):
-    return "删除成功"
-  else:
-    return "删除失败"
+    if user.user_delete(item.uid):
+        return "删除成功"
+    else:
+        return "删除失败"
