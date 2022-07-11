d3ae96d0585e5e544eb1609dc27bf159fea79480
sherlockjohnson22333@gmail.com <sherlockjohnson22333@gmail.com>
2022-07-11 16:04:30 +0800

    数据获取修改

--- back-end/api/v1/endpoints/mineral.py
+++ back-end/api/v1/endpoints/mineral.py
@@ -5,20 +5,6 @@ from models.db import con
 mineral = APIRouter(tags=["矿产资源"])
 
 
-# # 按列表方式返回所有数据
-# @mineral.get("/mineralAll", summary="所有矿产数据")
-# def get_all():
-#     sql_result = con.execute(f'SELECT * FROM mineral_resources')
-#     return sql_result.all()
-
-
-# # 根据获取到的省份名称按列表方式返回对应数据
-# @mineral.get("/mineralProvince", summary="省市矿产数据")
-# def get_single(province):
-#     sql_result = con.execute(
-#         f'SELECT * FROM mineral_resources WHERE province=\'{province}\'')
-#     return sql_result.all()
-
 # 按列表方式返回所有数据
 @mineral.get("/mineralAll", summary="所有矿产数据")
 def get_all():
@@ -40,7 +26,7 @@ def get_single(province):
 @mineral.get("/mineralType", summary="指定矿产数据")
 def get_type(type):
     sql_result = con.execute(
-        f'SELECT province, `{type}` FROM mineral_resources ORDER BY ABS(`{type}`) DESC')
+        f'SELECT province, `{type}` as "value" FROM mineral_resources ORDER BY ABS(`{type}`) DESC')
     data = [dict(zip(result.keys(), result)) for result in sql_result]
     return data
 
