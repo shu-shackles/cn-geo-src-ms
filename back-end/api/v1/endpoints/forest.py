dcdd85130496fcf6828e0991b05776e52086a204
sherlockjohnson22333@gmail.com <sherlockjohnson22333@gmail.com>
2022-07-18 10:50:43 +0800

    修改返回参数类型

--- back-end/api/v1/endpoints/forest.py
+++ back-end/api/v1/endpoints/forest.py
@@ -9,7 +9,7 @@ forest = APIRouter(tags=["森林资源"])
 @forest.get("/forestAll", summary="所有森林数据")
 def get_all():
     sql_result = con.execute(
-        f'SELECT * FROM forest ORDER BY ABS(`forest coverage`) DESC')
+        f'SELECT `province/company` AS "省市或公司", ABS(`forest coverage`) AS "森林覆盖面积", ABS(`increase percentage points`) AS "森林覆盖率", ABS(`natural forest area`) AS "自然林面积", ABS(`plantation area`) AS "人工林面积" FROM forest ORDER BY ABS(`forest coverage`) DESC')
     data = [dict(zip(result.keys(), result)) for result in sql_result]
     return data
 
@@ -18,7 +18,7 @@ def get_all():
 @forest.get("/forestProvince", summary="省市森林数据")
 def get_single(province):
     sql_result = con.execute(
-        f'SELECT * FROM forest WHERE `province/company`=\'{province}\'')
+        f'SELECT `province/company` AS "省市或公司", ABS(`forest coverage`) AS "森林覆盖面积", ABS(`increase percentage points`) AS "森林覆盖率", ABS(`natural forest area`) AS "自然林面积", ABS(`plantation area`) AS "人工林面积" FROM forest WHERE `province/company`=\'{province}\'')
     data = [dict(zip(result.keys(), result)) for result in sql_result]
     return data
 
@@ -27,6 +27,6 @@ def get_single(province):
 @forest.get("/forestType", summary="指定森林数据")
 def get_type(type):
     sql_result = con.execute(
-        f'SELECT `province/company`, `{type}` as "value" FROM forest ORDER BY ABS(`{type}`) DESC')
+        f'SELECT `province/company`, ABS(`{type}`) as "value" FROM forest ORDER BY ABS(`{type}`) DESC')
     data = [dict(zip(result.keys(), result)) for result in sql_result]
     return data
